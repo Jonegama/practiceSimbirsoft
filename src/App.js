abf531c5e74ed5c9b9dc99d1992426014a792028
Jonegama <gofman123@list.ru>
2022-07-16 02:43:14 +0400

    #3 V-01. Начало верстки.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 
 function App() {
   return (
-    <div className="App">
+    <div className="main inline">
+      
+      <div className="left-block">
+        <button onClick="" className="button-left-block">
+          <a className="button-left-text">Eng</a>
+        </button>
+      </div>
+
+      <div className="content">
         <div>
-          <h1>Hellow World!</h1>
-          <p>Test</p>
+          <h3>Need for drive</h3>
+          <div>
+            <img></img>
+            <p>Ульяновск</p>
+          </div>
         </div>
+        <div className="content-center">
+          <h1>Каршеринг</h1>
+          <h1>Need for drive</h1>
+          <p>Поминутная аренда авто твоего города</p>
+        </div>
+        <button className="button-center" title="Забронировать"></button>
+        <div>
+          <p>© 2016-2019 «Need for drive»</p>
+          <p>8 (495) 234-22-44</p>
+        </div>
+      </div>
+
+      <div className="right-block">
+        <h2>Бесплатная парковка</h2>
+        <p>Оставляйте машину на платных городских 
+           парковках и разрешенных местах, не нарушая ПДД,
+           а также в аэропортах.
+        </p>
+        <button className="" title="Подробнее"></button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './Components/style/styles.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,4 +11,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals(); 
